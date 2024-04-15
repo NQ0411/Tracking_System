9da66794454403b714649d24286ef3f994cf9c62
NQ0411 <167076301+NQ0411@users.noreply.github.com>
2024-04-15 19:05:13 +0700

    Add files via upload

---
+++ inc/servo_cf.h
@@ -0,0 +1,14 @@
+/*
+ * servo_cf.h
+ *
+ *  Created on: Aug 7, 2023
+ *      Author: ADMIN
+ */
+
+#ifndef INC_SERVO_CF_H_
+#define INC_SERVO_CF_H_
+
+#include "servo.h"
+extern const SERVO_CfgType SERVO_CfgParam[SERVO_NUM];
+
+#endif /* INC_SERVO_CF_H_ */
